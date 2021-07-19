864ad7988014b34fa528e710518d1f6a79a5e12d
trisub49 <trisub49@gmail.com>
2021-07-19 06:57:58 +0200

    another method

--- fermat.php
+++ fermat.php
@@ -9,39 +9,45 @@
 <body>
 
   <?php
-    
-    echo 'Összesen '.countPrimes(1000000).'db találat.';
+    ini_set('max_execution_time', '0');
 
-    function countPrimes($max) {
-      $counter = 0;
+    countCases(1000);
+    
+    function countCases($max) {
       $primes = array();
-      $maxPrime = 0;
+      $sum = 0;
+
       for($i = 1; $i < $max; $i ++) {
         if(gmp_nextprime($i) <= $max) {
           if(gmp_nextprime($i - 1) == $i) {
-            array_push($primes, $i);
+            array_push($primes, gmp_intval($i));
           } else {
             $i = gmp_nextprime($i);
-            array_push($primes, $i);
+            array_push($primes, gmp_intval($i));
           }
           $maxPrime = $i;
-          //echo $i.' ';
-        } else $i = $max;
+        } else $i = $max; 
       }
-      for($a = 0; $a < count($primes); $a ++) {
-        for($b = $a; $b < count($primes); $b ++) {
-          $sum = floatval(pow($primes[$a], 3) + pow($primes[$b], 3));
-          $c = floatval(pow($sum, 1/3));
-          // echo $sum.' - '.$c.'<br>';
-          if($sum > pow($maxPrime, 3)) {
-            $a = count($primes);
-            $b = count($primes);
-          }
-          if(floor($c) == $c && in_array($c, $primes)) {
-            $counter ++;
-          }
+
+      for($i = 0; $i < count($primes); $i ++) {
+        $sum += F($primes[$i]);
+      }
+
+      echo 'Összesen '.$sum.' lehetőség van '.$max.' (p) alatti prímszámok esetén.';
+    }
+
+    function F($p) {
+      $counter = 0;
+      for($a = 1; $a < $p; $a ++) {
+        for($b = 1; $b < $p; $b ++) {
+          for($c = 1; $c < $p; $c ++) {
+            if((pow($a, 3) + pow($b, 3)) % $p == pow($c, 3) % $p) {
+              $counter ++;
+            }
+          } 
         }
       }
+      // echo 'F('.$p.')=+ '.$counter.'<br>';
       return $counter;
     }
   ?>
